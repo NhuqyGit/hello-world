4cd5308d62f84663dc35688dbf0cf2e65735d077
NhuqyGit <kathonFLAG@gmail.com>
2021-09-24 00:11:54 +0700

    Add test.cpp file

---
+++ test.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+//Implement the class Box  
+//l,b,h are integers representing the dimensions of the box
+class Box{
+    private:
+        int l, b, h;
+        //ostream& operator<<(ostream& out, Box& B);
+    public:
+        Box(): l(0), b(0), h(0){}
+        Box(int length, int breadth, int height):l(length), b(breadth), h(height){}
+        Box(const Box& B){
+            this->l = B.l;
+            this->b = B.b;
+            this->h = B.h;
+        };
+        int getLength() const{
+            return l;
+        }
+        int getBreadth() const{
+            return b;
+        }
+        int getHeight() const{
+            return h;
+        }
+        
+        long long CalculateVolume(){
+            return (long long)l*b*h;
+        }
+        bool operator<(Box& other){
+          bool condition1 = l < other.l;
+          bool condition2 = b < other.b && l == other.l;
+          bool condition3 = h < other.h && b == other.b && l == other.l;
+          if(condition1 || condition2 || condition3)
+            return true;
+          return false;
+        }
+        friend ostream& operator<<(ostream& out, Box& B){
+            out << B.l << " " << B.b << " " << B.h;
+            return out;
+        }
+};
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
